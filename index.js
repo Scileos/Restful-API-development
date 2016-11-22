f9877cfe5673c25113b38cc67017815bdf526ad7
moorej22 <00jonniem@gmail.com>
2016-11-22 14:17:52 +0000

    Actual tests

--- index.js
+++ index.js
@@ -18,31 +18,7 @@ server.listen(port, function(err) {
 }
 )
 
-function validateJson(json) {
-	if (json === undefined) {
-		//console.log('UNDEFINED')
-		return false
-	}
-	if (typeof json.name !== 'string') {
-		//console.log(`NAME NOT STRING`)
-		return false
-	}
-	/* returns false if the list key is not an Array */
-	if (!Array.isArray(json.list)) {
-		//console.log('LIST NOT ARRAY')
-		return false
-	}
-	/*  */
-	//for(let i=0; i<json.list.length; i++) {
-	//	if (typeof json.list[i] !== 'string') {
-	//		console.log('ARRAY INDEX NOT STRING')
-	//		return false
-	//	}
-	//}
-	for (const item of json.list) if ( typeof item !== 'string') return false
-	/* otherwise return true */
-	return true
-}
+
 
 /** test GET request 
 	server.get ('/Ingredient/Nutrition', function(req, res) {
--- spec/FatSecret-spec.js
+++ spec/FatSecret-spec.js
@@ -3,9 +3,18 @@ const API = require ('../Public/FatSecret API - Nutrition.js')
 
 
 describe('Fat Secret', function() {
-	it('should return data on a GET request', () => {
+
+	it('should return food_id on a GET request', () => {
 		API.GetRequest().then((request) => {
 			expect(request).toBe('Strained Noodles and Chicken Vegetables Dinner Babyfood')
 		})
 	})
+
+	it('Should fail if the food_id is not valid '), () => {
+		API.GetRequest().then((request) => {
+			expect(request).toBe(Error)
+		})
+	}
+
+
 })
