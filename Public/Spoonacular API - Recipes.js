0b003e5ca4b7fe496f21f16346688b02f2e925f8
moorej22 <00jonniem@gmail.com>
2016-11-28 12:21:49 +0000

    Spoon branch

--- Public/Spoonacular API - Recipes.js
+++ Public/Spoonacular API - Recipes.js
@@ -1,3 +1,25 @@
 'use strict'
 
-const SP_Key = ''
+const SP_Key = 'hSYPFdrWE8mshIwh8iTx7DXbAbjap1dU9pijsnGTR808aCq70h'
+const rest = require ('restler')
+const SP_URL = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com'
+
+//Get recipe by ingredient
+const reqObject = {
+	fillIngredients: 'false',
+	ingredients: '',
+	limitLicense: 'false',
+	number: '15',
+	ranking: '1'
+}
+
+exports.GetRecipes = (ingredient) => {
+	reqObject.ingredients = ingredient
+	rest.get(SP_URL, {
+		data: reqObject
+	}) .on('complete', function(data) {
+		console.log(data)
+	})
+}
+
+//Get Analyze a recipe search for ingredients
--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const restify = require('restify')
 const server = restify.createServer()
 const FS_API = require ('./Public/FatSecret API - Nutrition.js')
+const SP_API = require ('./Public/Spoonacular API - Recipes.js')
 
 const defaultPort = 3000
 
@@ -25,4 +26,16 @@ server.get('/ingredient/:ingredientID', function(req, res) {
 		res.send(result)
 		res.end
 	})
+})
+
+server.get('recipe/:ingredient', function(req, res) {
+	const ingredient = req.params.ingredient
+	req.setHeader('X-Mashape-Key', SP_API.SP_Key)
+	req.setHeader('Accept', 'application/json')
+	const result = SP_API.GetRecipes(ingredient)
+	res.setHeader('content-type', 'application/json')
+	res.setHeader('Allow', 'GET')
+	res.send(result)
+	res.end
+
 })
