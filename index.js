8cf100bda285e6af6178de4256e209a65aad3f3d
moorej22 <00jonniem@gmail.com>
2016-12-14 04:49:40 +0000

    Final changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const register = require('./Public/register.js')
 const auth = require('./Public/auth.js')
 const favourites = require('./Public/Favourites.js')
 
-const defaultPort = 8080
+const defaultPort = 3000
 
 
 /** Create server listening on port 3000 */
